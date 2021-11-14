0b40cdc494b17c1eb6c224c0e894a70212ee7e78
CodexpathCommunity <codexpath3@gmail.com>
2021-11-14 17:29:39 +0100

    configured prettier and eslint to work together

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,52 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 // eslint-disable-next-line func-names
 const App = function (): JSX.Element {
-  const [count, setCount] = useState(1);
+    const [count, setCount] = useState(1)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={() => setCount((inCount) => inCount + 1)}
-          >
-            count is:
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          <code>App.tsx</code>
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-};
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello Vite + React!</p>
+                <p>
+                    <button
+                        type="button"
+                        onClick={() => setCount((inCount) => inCount + 1)}
+                    >
+                        count is:
+                        {count}
+                    </button>
+                </p>
+                <p>
+                    Edit
+                    <code>App.tsx</code>
+                    and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    )
+}
 
-export default App;
+export default App
